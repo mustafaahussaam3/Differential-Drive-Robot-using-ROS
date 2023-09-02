bcf6ad9fb31f930d5d4991d8ddec8abbadb86439
mustafaahussaam3 <mustafahussameldin@gmail.com>
2023-09-02 12:24:11 +0300

    Editing File system

--- urdf_description/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mustaf/Documents/ROS_Course/Workspace/urdf_description/src
-# Build directory: /home/mustaf/Documents/ROS_Course/Workspace/urdf_description/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("robot1")
-subdirs("test")
--- urdf_description/build/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mustaf/Documents/ROS_Course/Workspace/urdf_description/src/test
-# Build directory: /home/mustaf/Documents/ROS_Course/Workspace/urdf_description/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
