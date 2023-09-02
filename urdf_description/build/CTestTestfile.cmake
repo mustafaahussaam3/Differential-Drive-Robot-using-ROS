224631eb483ca558b1057dbb1ab1f217e4515cc2
mustafaahussaam3 <mustafahussameldin@gmail.com>
2023-09-02 12:17:59 +0300

    Designing Basic Robot using ROS and Gazebo Classic

---
+++ urdf_description/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/mustaf/Documents/ROS_Course/Workspace/urdf_description/src
+# Build directory: /home/mustaf/Documents/ROS_Course/Workspace/urdf_description/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("robot1")
+subdirs("test")
---
+++ urdf_description/build/test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mustaf/Documents/ROS_Course/Workspace/urdf_description/src/test
+# Build directory: /home/mustaf/Documents/ROS_Course/Workspace/urdf_description/build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
